32987692a9e336b5c0503e7eeb15589779e6a353
Iago Gois <iagorafaelbg@gmail.com>
2022-11-25 15:24:05 -0300

    criado diretorio do exercicio do dia 4.2 e primeiros exercicios pre ao vivo

---
+++ 01-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/arrays-listas/script.js
@@ -0,0 +1,12 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu.indexOf('Serviços');
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+// console.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+console.log(menu);
