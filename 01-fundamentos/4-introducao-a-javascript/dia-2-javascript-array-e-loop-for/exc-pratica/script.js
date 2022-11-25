e093b1cb22701f777fb7aef824f63f3632ba34a0
Iago Gois <iagorafaelbg@gmail.com>
2022-11-25 18:55:32 -0300

    exc-pratica percorrido o array, somado todos os valores, estutura if > 20 e agora imprimir o maior valor

--- 01-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exc-pratica/script.js
+++ 01-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exc-pratica/script.js
@@ -1,12 +1,29 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// tem como fazer com for/of?
+// tem como fazer com for/of? nao tem, tem que usar o grande. esse resumido so server pra percorrer o array todo
 // for(let sum of numbers){
-//     sum = sum + numbers
-// };
+    //     sum += numbers[?]
+    // };
+    
+    
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
 for(let i = 0; i < numbers.length; i += 1){
-    sum += numbers[i];
-    // sum = numbers[i] + sum; -- igual ao de cima
-};
-console.log(sum);
+    if(numbers[i] > sum){
+        sum = numbers[i];
+    }else{};
+}
+console.log('O maior numero contido em "numbers" e: ' + sum + '.');
+// for(let i = 0; i < numbers.length; i += 1){
+//     sum += numbers[i];
+//     // sum = numbers[i] + sum; -- igual ao de cima
+// };
+
+// let media = sum / numbers.length;
+
+// if(media > 20){
+//     console.log('Valor maior que 20!');
+// }else{
+//     console.log('Valor Menor ou Igual a 20!');
+// }
+
+// console.log('O resultado da soma e: ' + sum + '. E o resultado da media e: ' + media);
