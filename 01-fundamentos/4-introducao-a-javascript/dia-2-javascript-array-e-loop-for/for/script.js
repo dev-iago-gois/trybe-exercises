a59ae62425bbde6afcbdf3fdbcb07537b9a6bb81
Iago Gois <iagorafaelbg@gmail.com>
2022-11-25 15:51:21 -0300

    exc-for

---
+++ 01-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/for/script.js
@@ -0,0 +1,18 @@
+// tabuada
+// let numero = 7;
+// for(let index = 1; index <= 9; index++){
+//     console.log(numero * index);
+// };
+
+// let listaDeNomes = ['Joana ','Maria','Joao'];
+
+// for (let index = 0; index < listaDeNomes.length; index += 1) {
+//     let mensagem = 'Boas vindas, ' + listaDeNomes[index] + '!';
+//     console.log(mensagem)
+// };
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < groceryList.length; i += 1){
+    console.log(groceryList[i]);
+};
