770437169e2f90600f9af8d6fe0dbb313e1a5ca2
Iago Gois <iagorafaelbg@gmail.com>
2022-12-02 23:44:47 -0300

    Exc - Bonus - quadrado invertido feito com muita ajuda do gabarito

--- 01-fundamentos/4-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exc-bonus.js
+++ 01-fundamentos/4-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exc-bonus.js
@@ -1,16 +1,16 @@
 let n = 5;
-let star = '*';
-let input = '';
+let star = '●';
+let inputLine= '';
+let inputPos = n - 1;
 for (let index = 0; index < n; index += 1) {
-    input = input + star;
-    console.log(input);
+    for (let indexColun = 0; indexColun < n; indexColun += 1) {
+        if (indexColun < inputPos) {
+            inputLine = inputLine + ' ';
+        }else{
+            inputLine = inputLine + star;
+        }
+    }
+    console.log(inputLine);
+    inputLine = '';
+    inputPos -= 1;
 };
-// for (let index = 0; index < n; index+= 1) {
-//     console.log(input);
-    
-// };
-
-
-
-
-console.log(input);
