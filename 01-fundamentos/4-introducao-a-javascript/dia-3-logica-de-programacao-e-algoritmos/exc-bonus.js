92168104c74b054a11dafbf3bed2af4a28ec6713
Iago Gois <iagorafaelbg@gmail.com>
2022-12-02 23:17:13 -0300

    Exc - Bonus - quadrado de 5* done sozinho, bug corrigido com gabarito. piramide base 5 done sozinho.

---
+++ 01-fundamentos/4-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exc-bonus.js
@@ -0,0 +1,16 @@
+let n = 5;
+let star = '*';
+let input = '';
+for (let index = 0; index < n; index += 1) {
+    input = input + star;
+    console.log(input);
+};
+// for (let index = 0; index < n; index+= 1) {
+//     console.log(input);
+    
+// };
+
+
+
+
+console.log(input);
